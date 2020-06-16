2d339f9a1fb520fe7ba7d445fb82c3a28a3a6ceb
Tiffany <chin.tiffany108@gmail.com>
2020-06-16 15:59:12 -0400

    separate navbar component

--- frontend/src/components/splash/navbar.jsx
+++ frontend/src/components/splash/navbar.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import '../../stylesheets/splash.scss';
-import Navbar from './navbar.jsx';
 
-class Splash extends React.Component {
+class Navbar extends React.Component {
   constructor(props) {
     super(props);
-
   }
- 
+
   render() {
     const { openModal, isAuthenticated, logout } = this.props;
 
@@ -58,8 +55,8 @@ class Splash extends React.Component {
         </section>
 
       </section>
-    ) 
+    )
   }
 };
 
-export default withRouter(Splash);
+export default withRouter(Navbar);
