873e469169100b6b960d365da46c9b46c87c5f53
Tiffany <chin.tiffany108@gmail.com>
2020-06-17 16:03:53 -0400

    find workouts form

--- frontend/src/components/training/training.jsx
+++ frontend/src/components/training/training.jsx
@@ -16,8 +16,50 @@ class Training extends React.Component {
   }
 
   render() {
-    let equipmentCheckbox = [];
-    const equipment = []
+    let equipmentCheckbox, muscleGroupsCheckbox;
+    const equipment = [
+      "Barbell", 
+      "Barbell Rack", 
+      "Bench", 
+      "Benchpress Machine", 
+      "Chair", 
+      "Dip Handle Bars", 
+      "Dumbbell", 
+      "Lat Pulldown Machine", 
+      "Olympic Squat Rack", 
+      "Pullup Bar", 
+      "Tricep Pushdown Machine", 
+      "Weight Belt"
+    ];
+
+    equipmentCheckbox = equipment.map(e => (
+      <label>
+        <input type="checkbox"
+          value={e.toLowerCase()}/>{e}
+      </label>
+    ));
+
+    const muscleGroups = [
+      "Abdominals",
+      "Anterior Deltoid", 
+      "Bicep",
+      "Calves",
+      "Chest", 
+      "Gluteus Maximus",
+      "Hamstring",
+      "Latissimus Dorsi",
+      "Pectoral",
+      "Quadriceps",
+      "Trapizius",
+      "Tricep", 
+    ];
+
+    muscleGroupsCheckbox = muscleGroups.map(m => (
+      <label>
+        <input type="checkbox"
+          value={m.toLowerCase()}/>{m}
+      </label>
+    ))
 
     return (
       <section className="training">
@@ -30,53 +72,30 @@ class Training extends React.Component {
         <section className="main">
           <div className="form-container">
             <form className="training-form">
-              <div className="col-1">
-                <span> Please select all available equipment:</span>
-                <div className="equipment-list">
-                  <label>
-                    <input type="checkbox"/>Barbell
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Barbell Rack
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Bench
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Benchpress Machine
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Chair
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Dip Handle Bars
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Dumbbell
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Lat Pulldown Machine
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Olympic Squat Rack
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Pullup Bar
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Tricep Pushdown Machine
-                  </label>
-                  <label>
-                    <input type="checkbox"/>Weight Belt
-                  </label>
+              <div className="options">
+                <div className="col-1">
+                  <span> Please select all available equipment:</span>
+                  <div className="equipment-list">
+                    {equipmentCheckbox}
+                  </div>
+                </div>
+                <div className="col-2">
+                  <span>Please select targeted muscle groups:</span>
+                  <div className="muscles-list">
+                    {muscleGroupsCheckbox}
+                  </div>
+                </div>
+                <div className="col-3">
+                  <span>Choose your difficulty:</span>
+                  <select name="">
+                    <option value="">Beginner</option>
+                    <option value="">Intermediate</option>
+                    <option value="">Advanced</option>
+                  </select>
                 </div>
               </div>
-              <div className="col-2">
 
-              </div>
-              <div className="col-3">
-
-              </div>
+              <button>Let's Train</button>
             </form>
           </div>
         </section>
