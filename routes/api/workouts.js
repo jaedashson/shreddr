c2afbb7ebe4a9a97be838d0b45cba025b626696b
elee <ericlee.2311@gmail.com>
2020-06-17 15:32:19 -0400

    For Jae-Son to pull

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const Workout = require('../../models/Workout');
 const Exercise = require('../../models/Exercise');
 
+// POST /api/workouts/generate
+// params: muscleGroups, equipment, difficultyLevel
 router.post("/generate", (req, res) => {
     const difficultyLevel = ["beginner", "intermediate", "advanced"];
     const availEquip = [
@@ -30,24 +32,46 @@ router.post("/generate", (req, res) => {
     // Exercise.find( { muscleGroups : { $all: muscles }}).then( ex => res.json(ex))
 
     // WHAT WE'VE TRIED SO FAR
-    // Exercise.find().then(ex => res.json(ex)); // Gets all of the exercises
+    // Exercise.find().then(ex => res.json(ex)); 
+    // Gets all of the exercises
 
-    // Exercise.find().where({ muscleGroups: { $in : muscles } }).then( ex => res.json(ex)); // Gets empty array
-
-    // Exercise.find({
-    //     muscleGroups: req.body.muscleGroups
-    // }).then(ex => res.json(ex)).catch(err => res.json(err)); // Gets exact muscleGroup matches
+    // Exercise.find().where({ muscleGroups: { $in : muscles } }).then( ex => res.json(ex)); 
+    // Gets empty array
 
     Exercise.find({
-        muscleGroups: { $elemMatch: req.body.muscleGroups  }
-    }).then(ex => res.json(ex)).catch(err => res.json(err));
+        muscleGroups: req.body.muscleGroups
+    }).then(ex => res.json(ex)).catch(err => res.json(err)); 
+    // Gets exact muscleGroup matches
 
+    // Exercise.find({
+    //     muscleGroups: { $elemMatch: req.body.muscleGroups  }
+    // }).then(ex => res.json(ex)).catch(err => res.json(err)); 
+    // Returns error
+
+    // Exercise.
+    // Exercise.find({
+    //     muscleGroups: { $in: Exercise.muscleGroups }
+    // }).then(ex => res.json(ex)).catch(err => res.json(err));
 
 
     //   .where({ muscleGroups: { $in: muscles } })
     //   .then((ex) => res.json(ex));
     // const query = Exercise.find();
     // query.where(() => {return Exercise.equipment.includes(req.body.equipment)})
+
+    // Exercise.find()
+    //     .where('muscleGroups')
+    //     .in(req.body.muscleGroups)
+    //     .then(ex => res.json(ex))
+    //     .catch(err => res.json(err));
+
+    const myFind = field => {
+        if (Exercise[field].includes(req.body.field)){
+            return Exercise;
+        }
+    }
+
+    return myFind(muscleGroups).then(ex => res.json(ex));
 });
 
 module.exports = router;
