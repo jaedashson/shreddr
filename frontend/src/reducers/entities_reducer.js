6f6da16748b3a44f5075ba029616e7b3685c11e9
jlin96 <Jesse13234@gmail.com>
2020-06-19 11:38:10 -0400

    quick fix workout reducer removed

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import users from "./user_reducer";
-import bodyweights from './bodyweight_reducer';
 import exercises from './exercise_reducer';
 
 const entitiesReducer = combineReducers({
   users,
-  bodyweights,
   exercises
 });
 
