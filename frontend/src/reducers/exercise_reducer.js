9eab38245652600471da688cffff6e7c0dab6dfa
jlin96 <Jesse13234@gmail.com>
2020-06-19 11:56:51 -0400

    remove comment/style profile pic

--- frontend/src/reducers/exercise_reducer.js
+++ frontend/src/reducers/exercise_reducer.js
@@ -1,8 +1,8 @@
 import { RECEIVE_ALL_EXERCISES } from '../actions/exercise_actions';
 
 const ExerciseReducer = (state = {}, action) => {
+  
   Object.freeze(state);
-  // let newState = Object.assign({}, state);
   switch (action.type) {
     case RECEIVE_ALL_EXERCISES:
       return action.exercises.data;
